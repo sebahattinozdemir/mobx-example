027fbc1fd455fa112e5d67b570b6a18bf6cde800
Sebahattin ÖZDEMİR <sebahattinozdmr@gmail.com>
2022-04-08 09:29:33 +0300

    mobx-example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Observer, useLocalObservable } from "mobx-react";
+
+const StoreContext = React.createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalObservable(() => ({
+    bugs: ["centiped"],
+    addBug: (bug) => {
+      store.bugs.push(bug);
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    },
+  }));
+
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+
+const BugHeader = () => {
+  const store = React.useContext(StoreContext);
+  return <Observer>{() => <h1>Bug Count {store.bugsCount}</h1>}</Observer>;
+};
+
+const BugList = () => {
+  const store = React.useContext(StoreContext);
+  return (
+    <Observer>
+      {() => (
+        <ul>
+          {store.bugs.map((bug) => (
+            <li key={bug}>{bug}</li>
+          ))}
+        </ul>
+      )}
+    </Observer>
+  );
+};
+
+const BugsForm = () => {
+  const store = React.useContext(StoreContext);
+  const [bug, setBug] = React.useState("");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        store.addBug(bug);
+        setBug("");
+      }}
+    >
+      <input
+        type="text"
+        value={bug}
+        onChange={(e) => setBug(e.target.value)}
+      ></input>
+      <button type="submit">Add</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <div className="App">
+        <h1>Bugs</h1>
+        <BugHeader />
+        <BugList />
+        <BugsForm />
+      </div>
+    </StoreProvider>
   );
 }
 
